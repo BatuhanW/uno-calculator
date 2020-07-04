80399dce688bd39202a6294b33b393533eb84f73
Batuhan Wilhelm <batuhanwilhelm@gmail.com>
2020-07-04 15:35:50 +0300

    fix test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
+import { RecoilRoot } from "recoil";
 
 describe("App", () => {
   describe("Snapshot", () => {
     it("should match snapshot", async () => {
-      const { container } = render(<App />);
+      const { container } = render(
+        <RecoilRoot>
+          <App />
+        </RecoilRoot>
+      );
 
       expect(container).toMatchSnapshot();
     });
